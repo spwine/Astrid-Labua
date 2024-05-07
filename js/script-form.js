abd61f01578d06314b0c53e56f89275b0fd017ef
spwine <101738450+spwine@users.noreply.github.com>
2024-05-07 21:36:30 +0700

    contactus

--- js/script-form.js
+++ js/script-form.js
@@ -1,13 +1,22 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const inputs = document.querySelectorAll(".input-container input");
+  const inputs = document.querySelectorAll(
+    ".input-container input, .input-container textarea"
+  );
+
   inputs.forEach((input) => {
     input.addEventListener("focus", function () {
-      this.parentNode.querySelector("label").classList.add("active");
+      input.parentElement.querySelector("label").classList.add("active");
     });
+
     input.addEventListener("blur", function () {
-      if (!this.value) {
-        this.parentNode.querySelector("label").classList.remove("active");
+      if (input.value === "") {
+        input.parentElement.querySelector("label").classList.remove("active");
       }
     });
+
+    // Check if input has value on page load
+    if (input.value !== "") {
+      input.parentElement.querySelector("label").classList.add("active");
+    }
   });
 });
