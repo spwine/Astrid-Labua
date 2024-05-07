86ecde7de7c614266f93071078a14feed07359d4
spwine <101738450+spwine@users.noreply.github.com>
2024-05-07 20:46:06 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,28 +1,28 @@
-window.addEventListener("scroll", function () {
-  console.log("Scrolling");
-  var windowHeight = window.innerHeight;
-  var maxScroll = document.body.scrollHeight - windowHeight;
-  var scrollPosition = window.scrollY;
-  var footerPosition = footer.offsetTop;
+// window.addEventListener("scroll", function () {
+//   console.log("Scrolling");
+//   var windowHeight = window.innerHeight;
+//   var maxScroll = document.body.scrollHeight - windowHeight;
+//   var scrollPosition = window.scrollY;
+//   var footerPosition = footer.offsetTop;
 
-  console.log("Window Height:", windowHeight);
-  console.log("Max Scroll:", maxScroll);
-  console.log("Scroll Position:", scrollPosition);
-  console.log("Footer Position:", footerPosition);
+//   console.log("Window Height:", windowHeight);
+//   console.log("Max Scroll:", maxScroll);
+//   console.log("Scroll Position:", scrollPosition);
+//   console.log("Footer Position:", footerPosition);
 
-  // Smooth scroll when reaching the footer
-  if (scrollPosition >= maxScroll && scrollPosition < footerPosition) {
-    console.log("Smooth scrolling to footer");
-    var distanceToFooter = footerPosition - scrollPosition;
-    var smoothScrollAmount = distanceToFooter / 10; // Adjust the divisor for scrolling speed
+//   // Smooth scroll when reaching the footer
+//   if (scrollPosition >= maxScroll && scrollPosition < footerPosition) {
+//     console.log("Smooth scrolling to footer");
+//     var distanceToFooter = footerPosition - scrollPosition;
+//     var smoothScrollAmount = distanceToFooter / 10; // Adjust the divisor for scrolling speed
 
-    // Decelerate the scrolling speed
-    window.scrollTo({
-      top: window.scrollY + smoothScrollAmount,
-      behavior: "smooth",
-    });
-  }
-});
+//     // Decelerate the scrolling speed
+//     window.scrollTo({
+//       top: window.scrollY + smoothScrollAmount,
+//       behavior: "smooth",
+//     });
+//   }
+// });
 
 // copyright year
 
